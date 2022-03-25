d643fa97a10c2980662cc522ac347973b805548a
Ayush Soni <mr.soni1002001@gmail.com>
2022-03-25 23:59:02 +0530

    sending email in node

--- Practice/app.js
+++ Practice/app.js
@@ -1,33 +1,24 @@
-const formidable = require("formidable");
-const http = require("http");
-const fs = require("fs");
+var nodemailer = require("nodemailer");
 
-const server = http.createServer((req, res) => {
-  if (req.url == "/fileupload") {
-    let form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      res.write("<h1>File Uploaded</h1>");
-      let oldPath = files.filetoupload.filepath;
-      let newPath =
-        "D:/primary/MERN-stack-learning-program>" +
-        files.filetoupload.originalFilename;
-      fs.rename(oldPath, newPath, (err) => {
-        if (err) throw err;
-        res.write("File uploaded and moved!");
-        res.end();
-      });
-    });
-  } else {
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.write(
-      '<form action="fileupload" method="post" enctype="multipart/form-data">'
-    );
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write("</form>");
-    console.log("Data Displayed");
-    res.end();
-  }
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "youremail@gmail.com",
+    pass: "yourpassword",
+  },
 });
 
-server.listen(5000);
+var mailOptions = {
+  from: "youremail@gmail.com",
+  to: "myfriend@yahoo.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
--- Practice/uploadFile.js
+++ Practice/uploadFile.js
@@ -1,33 +1,30 @@
-const formidable = require("formidable");
-const http = require("http");
-const fs = require("fs");
+var http = require("http");
+var formidable = require("formidable");
+var fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url == "/fileupload") {
-    let form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      res.write("<h1>File Uploaded</h1>");
-      let oldPath = files.filetoupload.filepath;
-      let newPath =
-        "D:/primary/MERN-stack-learning-program>" +
-        files.filetoupload.originalFilename;
-      fs.rename(oldPath, newPath, (err) => {
-        if (err) throw err;
-        res.write("File uploaded and moved!");
-        res.end();
+http
+  .createServer(function (req, res) {
+    if (req.url == "/fileupload") {
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.filepath;
+        var newpath =
+          "C:/Users/Your Name/" + files.filetoupload.originalFilename;
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write("File uploaded and moved!");
+          res.end();
+        });
       });
-    });
-  } else {
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.write(
-      '<form action="fileupload" method="post" enctype="multipart/form-data">'
-    );
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write("</form>");
-    console.log("Data Displayed");
-    res.end();
-  }
-});
-
-server.listen(5000);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(
+        '<form action="fileupload" method="post" enctype="multipart/form-data">'
+      );
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit">');
+      res.write("</form>");
+      return res.end();
+    }
+  })
+  .listen(5000);
